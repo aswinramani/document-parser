221eddab2e28cacb0d7a0c4b95c8ee094ca128dc
Aswin Ramani <aswinramani5@gmail.com>
2026-07-06 03:07:07 +0530

    use state machine  to identify the tags and add insection for now

--- rust/src/parser/mod.rs
+++ rust/src/parser/mod.rs
@@ -5,12 +5,19 @@ use quick_xml::Reader;
 use quick_xml::events::Event;
 use crate::utils::clinical_sections::Section;
 
+#[derive(Debug, PartialEq)]
+enum ParseState {
+    Root,
+    InSection,
+}
+
 pub fn problem_section(file_path_str: &str) -> Section {
     let file_path = Path::new(file_path_str);
     let file = File::open(file_path).expect("Unable to open check if file or path exists!");
     let reader = BufReader::new(file);
     let mut xml = Reader::from_reader(reader);
     let mut buf = Vec::new();
+    let mut state = ParseState::Root;
 
     loop {
         match xml.read_event_into(&mut buf) {
@@ -19,14 +26,30 @@ pub fn problem_section(file_path_str: &str) -> Section {
             // works for opening tag for example <act>
             Ok(Event::Start(e)) => {
                 // println!("event start name {:?}", e.name());
+                match e.name().as_ref() {
+                    b"section" => state = ParseState::InSection,
+                    _ => {}
+                }
             }
             // works for closing tag for example </act>
             Ok(Event::End(e)) => {
                 // println!("event end name {:?}", e.name());
+                match e.name().as_ref() {
+                    b"section" => state = ParseState::Root,
+                    _ => {}
+                }
             }
             // works for self-closing tag for example <templateId/>
             Ok(Event::Empty(e)) => {
-                // println!("event empty name {:?}", e.name());
+                if state == ParseState::InSection {
+                    match e.name().as_ref() {
+                        b"templateId" => {
+                            let root = e.try_get_attribute(b"root");
+                            println!("templateId => root {:?}", root); 
+                        }
+                        _ => {}
+                    }
+                }
             }
             Ok(Event::Eof) => break,
             // skip other events
